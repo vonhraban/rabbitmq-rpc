5b07b643abec957e742a31914a81d80ae09bd73a
Vlad <hraban@vivec.co.uk>
2018-10-04 18:07:18 +0100

    Handle exceptions in the client endpoint

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,9 @@
 
 require_once '../vendor/autoload.php';
 
+use Datix\User\MalformedResponseException;
+use Datix\User\UserNotFoundException;
+use Datix\User\UserStore;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use Datix\User\RabbitMQUserStore;
@@ -11,15 +14,24 @@ require '../vendor/autoload.php';
 $app = require '../bootstrap.php';
 
 $app->get('/getUser/{id}', function (Request $request, Response $response, array $args) {
-    /** @var RabbitMQUserStore $rpc_connection */
+    /** @var UserStore $userStore */
     $userStore = $this->get('user_store');
-
     $id = $args['id'];
-    $response->withHeader('Content-type', 'application/json')
-        ->getBody()
-        ->write(json_encode($userStore->get($id)));
 
-    return $response;
+    $response = $response->withHeader('Content-type', 'application/json');
+    /** @var RabbitMQUserStore $rpc_connection */
+    try {
+        $user = json_encode($userStore->get($id));
+        return $response->withStatus(200)
+            ->getBody()
+            ->write($user);
+    } catch (UserNotFoundException $e) {
+        return $response->withStatus(404);
+    } catch (MalformedResponseException $e) {
+        return $response->withStatus(502);
+    } catch (\Exception $e) {
+        return $response->withStatus(500);
+    }
 });
 
 $app->run();
